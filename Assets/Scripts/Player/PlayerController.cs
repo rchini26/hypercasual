91c915146a52906f58925fb3d9f8dafd7a0d695b
rchini26 <rafa.montm@gmail.com>
2025-11-07 18:12:26 -0300

    Made Scene Prototype with Basic Elements

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,10 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // publics
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+    
     public float speed = 1f;
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckEndLine = "Finish";
+    public GameObject endScreen;
+    // privates
+    private bool _canRun;
+    private Vector3 _pos;
     
     void Update()
     {
+        if (!_canRun) return;
+        
+        var _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+        
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == tagToCheckEnemy)
+        {
+            EndGame();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == tagToCheckEndLine)
+        {
+            EndGame();
+        }
+    }
+    
+    void EndGame()
+    {
+        _canRun = false;
+        endScreen.SetActive(true);
+    }
+    
+    public void StartToRun()
+    {
+        _canRun = true;
     }
 }

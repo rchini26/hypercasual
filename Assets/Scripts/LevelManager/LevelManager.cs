6bf68bb8be1cdcf2ac9ddee052f1dc3aa4ad7e5c
rchini26 <rafa.montm@gmail.com>
2025-11-17 21:37:33 -0300

    Made LevelManager Script

---
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform container;
+    public List<GameObject> levelPrefabs;
+// Make a list of levels as prefabs 
+    [SerializeField] private int _index;
+    private GameObject _currentLevel;
+    void Awake()
+    {
+        SpawnNextLevel();
+    }
+    
+    void SpawnNextLevel()
+    {
+        // Check if there is a level
+        if (_currentLevel != null)
+        {
+            Destroy(_currentLevel);
+            _index++;
+            if (_index >= levelPrefabs.Count)
+            {
+                // Reset level count when all levels are completed
+                _index = 0;
+            }
+        }
+        // Instantiate a level inside the Level Container everytime
+        _currentLevel = Instantiate(levelPrefabs[_index], container.position, Quaternion.identity);
+        _currentLevel.transform.localPosition = Vector3.zero;   
+    }
+}

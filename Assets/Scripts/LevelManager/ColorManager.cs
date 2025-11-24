12783507c9e2e3846221bf879c365a864c3eed30
rchini26 <rafa.montm@gmail.com>
2025-11-23 22:09:50 -0300

    Created ChangeColor Script

---
+++ Assets/Scripts/LevelManager/ColorManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+  public List<Material> materials;
+  public List<ColorSetup> colorSetups;
+  
+  public enum ArtType
+  {
+    
+  }
+  
+  
+}
+
+public class ColorSetup
+{
+  public List<Color> colors;
+}
